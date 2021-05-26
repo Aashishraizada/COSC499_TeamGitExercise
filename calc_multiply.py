8049aaa8682b9e5c010a55a9c34d9be18f8edff0
Jarod <jarodalgra@gmail.com>
2021-05-26 08:40:13 -0700

    fix error catching

--- calc_multiply.py
+++ calc_multiply.py
@@ -1,12 +1,12 @@
 def mult():
-    a,b = None, None
+    a, b = None, None
     while True:
         a, b = input("Enter Two Numbers to Multiply: ").split()
         try:
             a = float(a)
             b = float(b)
             break
-        except TypeError:
+        except ValueError:
             print("You did not enter two numbers")
     product = a * b
     print(f'The Product of {a} and {b} is {product}')
